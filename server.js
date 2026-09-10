052ca2f4c6f889db2f385ade02e9ba44f51eb8cb
Lucas <135282384+LukkaG@users.noreply.github.com>
2026-09-10 18:53:41 -0300

    Corrige uso do hash da senha no registro

--- server.js
+++ server.js
@@ -27,7 +27,7 @@ app.post('/register', async (req, res) => {
         const saltRounds = 10;
         const hashedPassword = await bcrypt.hash(password, saltRounds);
 
-        const newUser = new User({ nickname, hashedPassword });
+        const newUser = new User({ nickname, password: hashedPassword });
         await newUser.save();
 
         await SaveData.create({
